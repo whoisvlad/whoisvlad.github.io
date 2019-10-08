2613ef7b7e36b6b5f004f538e788c189ceea9104
whoisvlad <vlad.crusade@gmail.com>
2019-10-08 17:34:25 +0300

    Minor fixes.

--- projects/Woxow/js/settings.js
+++ projects/Woxow/js/settings.js
@@ -19,8 +19,8 @@ $(".slider").slick({
             dots: false,
             arrows: false,
             infinite: false,
-            slidesToShow: 2,
-            slidesToScroll: 2
+            slidesToShow: 1,
+            slidesToScroll: 1
         } 
     }]
 });
