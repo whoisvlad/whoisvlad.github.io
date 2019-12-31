4d723c933dc35117f6637a21dc5b933c1835fb41
whoisvlad <vlad.crusade@gmail.com>
2019-12-31 11:35:56 +0200

    KKProd now with scss

---
+++ projects/KKProd/gulpfile.js
@@ -0,0 +1,65 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const browsersync = require("browser-sync").create();
+
+
+sass.compiler = require('node-sass');
+
+
+
+
+
+// BrowserSync
+function browserSync(done) {
+    browsersync.init({
+        server: {
+            baseDir: "./app",
+        },
+        port: 3000,
+        stream:true,
+        notify: false
+    });
+    done();
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
+
+// CSS task
+function css() {
+    return gulp
+            .src('./app/scss/**/*.scss')
+            .pipe(sass({
+                outputStyle: "expanded"
+            }))
+            .pipe(gulp.dest('./app/css/'));
+}
+
+
+
+// Watch files
+function watchFiles() {
+    gulp.watch("app/scss/**/*.scss", css), gulp.series(browserSyncReload);
+    gulp.watch(
+        [
+            "app/**/*"
+        ],
+        gulp.series(browserSyncReload)
+    );
+}
+
+
+// define complex tasks
+const build = gulp.series(css, browserSync);
+const watch = gulp.parallel(css, watchFiles, browserSync);
+
+
+
+// export tasks
+exports.css = css;
+exports.watch = watch;
+exports.default = watch;
